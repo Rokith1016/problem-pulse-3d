d7eb5a0f6a36b10e0674444738c496affc16bd4e
Rokith1016 <rokith466@gamail.com>
2025-12-23 00:07:07 +0530

    Move backend to root folder

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
+// Start server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
